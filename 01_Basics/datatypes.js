f244b1f75a7012a9b2c2b14ceedd166923ad7c62
Urvashi  Kushwaha <136898711+urvashi38@users.noreply.github.com>
2024-08-28 18:31:34 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,16 @@
+let name = "urvashi"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint = use for more big numbers
+// string = prefer to use in ""
+// boolean = true/false
+// null = standalone value
+// undefined =   
+// symbol = unique
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
