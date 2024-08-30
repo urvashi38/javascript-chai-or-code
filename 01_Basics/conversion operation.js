3b5075c4d36dfa0dc597227deb92c99519079cb0
Urvashi  Kushwaha <136898711+urvashi38@users.noreply.github.com>
2024-08-30 11:23:45 +0000

    conversion operation

--- 01_Basics/conversion operation.js
+++ 01_Basics/conversion operation.js
@@ -12,7 +12,17 @@
 //"33abc"= NaN
 // "true"= 1 : false = 0
 
-let isLoggedIn = 1
+let isLoggedIn = ""
 
-let booleanIsLoggedIn = boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 = true ; 0 = false
+// "" =  false
+// "hitesh" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
