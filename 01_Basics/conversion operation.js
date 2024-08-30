b3af4800c658b38cd36c3167293a1e2980d6519a
Urvashi  Kushwaha <136898711+urvashi38@users.noreply.github.com>
2024-08-30 05:00:17 +0000

    conversion operation

---
+++ 01_Basics/conversion operation.js
@@ -0,0 +1,18 @@
+//let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" = 33
+//"33abc"= NaN
+// "true"= 1 : false = 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
